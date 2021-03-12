c66fb7caa4fa66195084503059838bd4c2e0446a
barrymasjiduk <barrymasjiduk@gmail.com>
2021-03-12 19:27:52 +0000

    Update
    Update

--- clock/js/parse.js
+++ clock/js/parse.js
@@ -25,7 +25,9 @@
 	var ampm = hour >= 12 ? 'PM' : 'AM';
   	hour = hour % 12;
   	hour = hour ? hour : 12; // the hour '0' should be '12'
-	document.getElementById(salatId[i]).innerHTML = hour + ":" + minutes + " " + ampm;
+	var element = document.getElementById(salatId[i]);
+	element.innerHTML = hour + ":" + minutes + " " + ampm;
+	//document.getElementById(salatId[i]).innerHTML = hour + ":" + minutes + " " + ampm;
 	}
       }
     });
