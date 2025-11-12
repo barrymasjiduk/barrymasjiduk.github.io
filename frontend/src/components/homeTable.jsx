2c5b95d2ba4afd82874b7c289c33ad09cd54bc50
barrymasjiduk <barrymasjiduk@gmail.com>
2025-11-12 20:12:09 +0000

    add year to pdf download

--- frontend/src/components/homeTable.jsx
+++ frontend/src/components/homeTable.jsx
@@ -270,9 +270,9 @@ const currentSalahIndex = getCurrentSalahIndex();
           </Button>
           <Button
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
-            onClick={() => window.open("/november.pdf", "_blank")}
+            onClick={() => window.open("/november-2025.pdf", "_blank")}
           >
-            Download {monthOnly} Timetable
+            Download {monthOnly, year} Timetable
           </Button>
           {showThirdButton && (
             <Button
