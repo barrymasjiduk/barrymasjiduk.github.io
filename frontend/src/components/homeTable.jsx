eee19cd01c8c922a4bdff456ff9a8a5622444cbc
barrymasjiduk <barrymasjiduk@gmail.com>
2025-12-01 23:50:35 +0000

    Update PDF
    Update PDF

--- frontend/src/components/homeTable.jsx
+++ frontend/src/components/homeTable.jsx
@@ -270,7 +270,7 @@ const currentSalahIndex = getCurrentSalahIndex();
           </Button>
           <Button
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
-            onClick={() => window.open("/november-2025.pdf", "_blank")}
+            onClick={() => window.open("/December-2025.pdf", "_blank")}
           >
             Download {monthOnly} {yearOnly} Timetable
           </Button>
