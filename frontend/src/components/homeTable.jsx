83cf92238fb6c56cbceaeaae44d8c2ec846c5deb
barrymasjiduk <barrymasjiduk@gmail.com>
2026-02-02 18:41:06 +0000

    Change wording on Buttons
    Change wording on Buttons

--- frontend/src/components/homeTable.jsx
+++ frontend/src/components/homeTable.jsx
@@ -272,13 +272,13 @@ const currentSalahIndex = getCurrentSalahIndex();
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
             onClick={() => window.open("/February-2026.pdf", "_blank")}
           >
-            Download {monthOnly} {yearOnly} Timetable PDF
+            Download {monthOnly} {yearOnly} Timetable
           </Button>
           <Button
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
             onClick={() => window.open("/Ramadan-2026.pdf", "_blank")}
           >
-            Download Ramadan {yearOnly} Timetable PDF
+            Download Ramadan {yearOnly} Timetable
           </Button>
         </div>
         }
