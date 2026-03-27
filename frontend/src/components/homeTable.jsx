63cc363953b651322d0d990f389c3b373aeef889
barrymasjiduk <barrymasjiduk@gmail.com>
2026-03-27 19:44:30 +0000

    Update PDF and data.json
    Update PDF and data.json

--- frontend/src/components/homeTable.jsx
+++ frontend/src/components/homeTable.jsx
@@ -270,16 +270,18 @@ const currentSalahIndex = getCurrentSalahIndex();
           </Button>
           <Button
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
-            onClick={() => window.open("/March-2026-2-Sided.pdf", "_blank")}
+            onClick={() => window.open("/April-2026-2-Sided.pdf", "_blank")}
           >
-            Download {monthOnly} {yearOnly} Timetable
+            Download {monthOnly} {yearOnly} Timetable PDF
           </Button>
+          {showThirdButton && (
           <Button
             className="w-full sm:w-auto flex-1 md:text-sm text-[10px] whitespace-normal text-center px-2 py-2"
             onClick={() => window.open("/Ramadan-Fitrana-2026-2-Sided.pdf", "_blank")}
           >
             Download Ramadan {yearOnly} Timetable
           </Button>
+          )}
         </div>
         }
       </div>
